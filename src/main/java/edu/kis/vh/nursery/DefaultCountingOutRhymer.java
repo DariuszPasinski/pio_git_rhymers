19ecc1cd3bf2e930c8998689c0e838be6698cd79
kubamaz <kuba.adam.wojcik@gmail.com>
2025-03-22 18:52:57 +0100

    6.2 Poprawiono złe albo brakujące deklaracje stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,6 +5,7 @@ public class DefaultCountingOutRhymer {
     public static final int CAPACITY = 12;
     public static final int FULL_INDEX = CAPACITY - 1;
     public static final int EMPTY_VALUE = -1;
+    public static final int DEFAULT_RETURN_VALUE = -1;
     private final int[] numbers = new int[CAPACITY];
 
     public int total = EMPTY_VALUE;
@@ -24,13 +25,13 @@ public class DefaultCountingOutRhymer {
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return EMPTY_VALUE;
+                        return DEFAULT_RETURN_VALUE;
                     return numbers[total];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return EMPTY_VALUE;
+                            return DEFAULT_RETURN_VALUE;
                         return numbers[total--];
                     }
 
